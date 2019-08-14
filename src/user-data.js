c10265286fe0f7b216ed732afd70a87feab0668b
Michael.Andrews <Michael.Andrews@gamesys.co.uk>
2019-08-14 19:23:40 +0100

    Add Sign In page UI

---
+++ src/user-data.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "michael": {
+        "name": "Michael",
+        "email": "michael",
+        "password": "$2a$10$kDNQRgTYtLJpMBV5Zc5VP.XBwBvYFdcFHVpGYIcieQMEcPv2giF9e",
+        "permissions": "edit" //password
+    },
+    "viewer": {
+        "name": "Chris",
+        "email": "viewer",
+        "password": "$2a$10$ViHRTYhr7./RQYNmQpjkP.V5cnLtF9IWoFUUvPWnFvb3gLhaapQPG",
+        "permissions": "view" //ada
+    },
+    "restrict": {
+        "name": "Jon",
+        "email": "restrict",
+        "password": "$2a$10$pd.I8L8t5gGHOZK2zgTmnuQVtaBvj2u8YN9KNcYR2gFoD0Pw8Rtne",
+        "permissions": "restricted" //trees
+    }
+}
