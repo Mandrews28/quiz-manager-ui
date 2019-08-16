41e355b8963861df71c7b714202943e618b6bded
Michael.Andrews <Michael.Andrews@gamesys.co.uk>
2019-08-16 03:25:42 +0100

    Implement edit answer text and delete answer functionality

--- src/Components/AnswerHomePage/AnswerHomePage.js
+++ src/Components/AnswerHomePage/AnswerHomePage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './AnswerHomePage.css';
 import tick from '../../images/tick.png';
 import cross from '../../images/cross.png';
+import editIcon from '../../images/editIcon.png';
+import deleteIcon from '../../images/deleteIcon.png';
 
 class AnswerHomePage extends Component {
     constructor() {
@@ -55,7 +57,6 @@ class AnswerHomePage extends Component {
         if (this.state.permissions === 'edit') {
             answers.push(this.addAnswerDiv());
         }
-        console.log(this.state);
         this.setState({ answers });
     }
 
@@ -64,6 +65,15 @@ class AnswerHomePage extends Component {
             <div key={i} className="button-entry button-list list-entry">
                 <label>{answer.order}.</label>
                 <button type="button" className="button-list list-entry button-title list-entry-title">{answer.value}</button>
+                {this.state.permissions === 'edit' &&
+                    <span>
+                        <input type="image" src={deleteIcon} className="delete edit-button" alt="delete" onClick={(() => { this.onDeleteClick(answer) })} />
+                        <input type="image" src={editIcon} className="edit edit-button" alt="edit" onClick={(() => { this.onEditTextClick(answer, i) })} />
+                    </span>
+                }
+                {this.state.permissions !== 'edit' &&
+                    <span className="span-filler" />
+                }
             </div>
         )
     }
@@ -75,6 +85,88 @@ class AnswerHomePage extends Component {
                 <button type="button" className="button-list add-button button-title" onClick={this.onAddAnswerClick}>Add Answer</button>
             </div>
         )
+    }
+
+    onDeleteClick = (answer) => {
+        let result = window.confirm("Are you sure you want to delete answer " + answer.order + ": " + answer.value);
+        if (result) {
+            this.sendDeleteRequest(answer);
+        }
+    }
+
+    sendDeleteRequest = async (answer) => {
+        let quiz = this.state.quiz;
+        let question = this.state.question;
+
+        let url = 'http://localhost:8080/quiz/quiz' + quiz.order + '/question' + question.order + '/answer' + answer.order;
+
+        const response = await fetch(url, {
+            method: 'DELETE'
+        }).catch((e) => {
+            console.log(e);
+        });
+        await response.json().then(answerList => {
+            if (response.status === 200) {
+                this.setState({ answerList, answers: [] });
+                this.createListOfAnswerButtons();
+            } else {
+                alert(answerList.message);
+            }
+        });
+    }
+
+    onEditTextClick = (answer, index) => {
+        let answers = this.state.answers;
+        let oldAnswerDiv = answers[index];
+
+        let editForm = <form key={index} className="button-entry button-list list-entry" onSubmit={((event) => { this.postEditedText(event, answer) })}>
+            <label>{answer.order}.</label>
+            <input type="text" className="button-list list-entry button-title edit-button-title" defaultValue={answer.value} placeholder="Add your answer here" />
+            <img src={cross} className="cross edit-button" alt="cross" onClick={(() => { this.removeEditForm(oldAnswerDiv, index) })} />
+            <input type="image" src={tick} className="tick edit-button" alt="tick" />
+        </form>
+        answers[index] = editForm;
+        this.setState({ answers });
+    }
+
+    removeEditForm = (oldAnswerDiv, index) => {
+        let answers = this.state.answers;
+        answers[index] = oldAnswerDiv;
+        this.setState({ answers });
+    }
+
+    postEditedText = async (event, answer) => {
+        event.preventDefault();
+        let quiz = this.state.quiz;
+        let question = this.state.question;
+        let answerText = event.target[0].value;
+
+        if (answerText) {
+            const payload = {
+                value: answerText
+            };
+            let url = 'http://localhost:8080/quiz/quiz' + quiz.order + '/question' + question.order + '/answer' + answer.order;
+
+            const response = await fetch(url, {
+                method: 'PATCH',
+                body: JSON.stringify(payload),
+                headers: {
+                    'content-type': 'application/json',
+                }
+            }).catch((e) => {
+                console.log(e);
+            });
+            await response.json().then(answerList => {
+                if (response.status === 200) {
+                    this.setState({ answerList, answers: [] });
+                    this.createListOfAnswerButtons();
+                } else {
+                    alert(answerList.message);
+                }
+            });
+        } else {
+            alert("Please submit an answer");
+        }
     }
 
     onAddAnswerClick = () => {
